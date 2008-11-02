8207cb62000d331e0690269b4dd875b951f59947
areufer <areufer@4efe6d44-883f-0410-9ed0-acde54f1c0bb>
2008-11-02 13:46:33 +0000

    setup_gi works now
    git-svn-id: https://isis2.unibe.ch/repos/sphlatch@195 4efe6d44-883f-0410-9ed0-acde54f1c0bb

--- initials/setup_gi/setup_gi.m
+++ initials/setup_gi/setup_gi.m
@@ -5,26 +5,25 @@ lem = 3.4000e41;
 
 #mtar =  input("m_target   [me] = ");
 #mimp =  input("m_impactor [me] = ");
-mtar = 0.9;
-mimp = 0.1;
+mtar = 0.90;
+mimp = 0.10;
 mtar *= me;
 mimp *= me;
 
 #Rtar =  input("r_target   [Re] = ");
 #Rimp =  input("r_impactor [Re] = ");
-Rtar = 0.8;
-Rimp = 0.4;
+Rtar = 0.9;
+Rimp = 0.6;
 Rtar *= Re;
 Rimp *= Re;
 
 #vinf =  input("v_inf    [cm/s] = ");
 #Ltot  = input("L_tot     [lem] = ");
-vinf = 0.e6;
-Ltot = 1.00000;
+vinf = 0.0000e4;
+Ltot = 1.520000;
 Ltot *= lem;
 
 # determine impact parameters
-
 mtot = mtar + mimp;
 gamma = mimp / mtot;
 
@@ -42,29 +41,100 @@ bscal = Ltot / Lgraz;
 betaimp = acos( bscal );
 
 
-printf("\n parameters summary:\n\n");
+printf("\n parameters:\n");
 printf("gamma = %f\n", gamma);
 printf("vgraz = %e cm/s\n", vgraz);
 printf("Lgraz = %f lem\n", Lgraz/lem);
-printf("bscal = %f  (impact angle = %f°)\n", bscal, betaimp*360/(2*pi) );
-printf("vimp  = %e cm/s\n", vimp);
+printf("bscal = %f \n", bscal);
 printf("vesc  = %e cm/s\n", vesc);
 
 # total energy
 Etot = ( (vimp^2.) / 2. ) - ( mu / rimp );
 
-
 # determine impactor orbit eccentricity and true anomaly of impact
 e = sqrt( ((k1 - 1)*cos(betaimp))^2. + (sin(betaimp))^2. );
-thetaimp = atan(( k1 * sin(betaimp) * cos(betaimp) ) 
-  / ( k1 * (cos(betaimp) ^2. ) - 1.) );
+
+thetaimp = atan2(( k1 * sin(betaimp) * cos(betaimp) ),
+( k1 * (cos(betaimp) ^2. ) - 1.) );
 
 printf("e     = %f\n", e);
-printf("theta = %f° (impact true anomaly)\n", thetaimp*360/(2*pi));
+
+# determine perigee radius and velocity
+rperih = rimp *( 1. + e*cos(thetaimp) ) / ( 1. + e );
+vperih = sqrt( vinf^2. + ( 2.*mu / rperih ) );
+printf("\n perihelon:\n");
+printf("r     = %e cm/s\n", rperih );
+printf("v     = %e cm/s\n", vperih);
+printf("theta = %f° (true anomaly)\n", 0.);
+
+printf("\n impact:\n");
+printf("r     = %e cm\n", rimp);
+printf("v     = %e cm/s\n", vimp);
+printf("theta = %f° (true anomaly)\n", thetaimp*360/(2*pi));
+printf("beta  = %f° (impact angle)\n", betaimp*360/(2*pi));
+
+# determine true anomaly theta0 for r0 = x*rimp
+r0 = 5.*rimp;
+v0 = sqrt( vinf^2. + ( 2.*mu / r0 ) );
+theta0 = acos( ( rperih*(1. + e ) - r0 ) / ( e * r0 ) );
+beta0 = acos( Ltot / ( r0*v0*mimp ) );
+
+printf("\n initial setup:\n");
+printf("( r0 = %f rimp )\n", r0 / rimp);
+printf("r     = %e cm\n", r0);
+printf("v     = %e cm/s\n", v0);
+printf("theta = %f° (true anomaly)\n", theta0*360/(2*pi));
+printf("beta  = %f° \n", beta0*360/(2*pi));
+
+# initial velocity vector
+r0vect(1) = -r0* cos( (pi/2.) - thetaimp + theta0 );
+r0vect(2) =  r0* sin( (pi/2.) - thetaimp + theta0 );
+
+alpha = theta0 - thetaimp - beta0;
+v0vect(1) = -v0 * cos(alpha);
+v0vect(2) = -v0 * sin(alpha);
+
+# now place everything, so that center of mass is (0,0)
+# and there is no resulting momentum
+
+rimp0(1) =  ( mtar / mtot )*r0vect(1);
+rimp0(2) =  ( mtar / mtot )*r0vect(2);
+vimp0(1) =  ( mtar / mtot )*v0vect(1);
+vimp0(2) = -( mtar / mtot )*v0vect(2);
+
+rtar0(1) = -( mimp / mtot )*r0vect(1);
+rtar0(2) = -( mimp / mtot )*r0vect(2);
+vtar0(1) = -( mimp / mtot )*v0vect(1);
+vtar0(2) =  ( mimp / mtot )*v0vect(2);
+
+
+# determine time to perihelon for impact and for initial setup
+
+if e > 1.
+  a = rperih / ( e - 1. );
+  k2 = sqrt( mu / (a^3. ) );
+  
+  timp = ( (e*sqrt(e^2.-1.)*sin(thetaimp))/(1. + e*cos(thetaimp) ) -
+    log( (sqrt(e^2.-1.) + (e - 1.)*tan( thetaimp / 2. ))/
+         (sqrt(e^2.-1.) - (e - 1.)*tan( thetaimp / 2. )) ) ) / k2;
+  
+  t0 = ( (e*sqrt(e^2.-1.)*sin(theta0))/(1. + e*cos(theta0) ) -
+    log( (sqrt(e^2.-1.) + (e - 1.)*tan( theta0 / 2. ))/
+         (sqrt(e^2.-1.) - (e - 1.)*tan( theta0 / 2. )) ) ) / k2;
+else
+  k2 = sqrt( mu / ( 8. * rperih^3.) );
+
+  timp = abs(0.5*(tan(thetaimp / 2.) + (1./3.)*((tan(thetaimp / 2.))^3.)) / k2);
+  t0   = abs(0.5*(tan(theta0   / 2.) + (1./3.)*((tan(theta0   / 2.))^3.)) / k2);
+end
+
+printf("t     = %f s\n", timp - t0);
 printf("\n");
 
-# determine 
-
-
-#
+printf(" commands to setup the initial file:\n");
+printf("h5part_displace -i impactor.h5part --pos [%e,%e,%e] --vel [%e,%e,%e] --id 2000000 \n", rimp0(1), 0., rimp0(2), vimp0(1), 0., vimp0(2));
+printf("h5part_displace -i target.h5part --pos [%e,%e,%e] --vel [%e,%e,%e]\n", rtar0(1), 0., rtar0(2), vtar0(1), 0., vtar0(2));
+printf("h5part_combine -a target.h5part -b impactor.h5part -o combined.h5part\n");
+printf("h5part_writeattr -i combined.h5part -k time -v=%e\n", timp - t0 );
+printf("\n");
 
---
+++ initials/setup_gi/test_orbit.m
@@ -0,0 +1,76 @@
+setup_gi
+
+parts.pos(1,1) = rimp0(1);
+parts.pos(2,1) = rimp0(2);
+parts.vel(1,1) = vimp0(1);
+parts.vel(2,1) = vimp0(2);
+parts.m(1) = mimp;
+
+parts.pos(1,2) = rtar0(1);
+parts.pos(2,2) = rtar0(2);
+parts.vel(1,2) = vtar0(1);
+parts.vel(2,2) = vtar0(2);
+parts.m(2) = mtar;
+
+parts.acc = zeros(2,2);
+
+parts_orig = parts;
+
+tabs = timp - t0;
+ttot = 1.10*abs(tabs);
+
+
+nsteps = 1000;
+dt = ttot / nsteps;
+pos = zeros(nsteps, 4);
+dist = zeros(nsteps, 1);
+vrel = zeros(nsteps, 1);
+t    = zeros(nsteps, 1);
+
+hit = false;
+hitstep = nsteps;
+
+for step = 1:nsteps
+  parts = bf_nbody(parts, G, dt);
+
+  pos(step,:) = [ parts.pos(:,1); parts.pos(:,2) ];
+
+  rvect = parts.pos(:,1) - parts.pos(:,2);
+  dist(step) = sqrt( sum( rvect .* rvect ) );
+  vvect = parts.vel(:,1) - parts.vel(:,2);
+  vrel(step) = sqrt( sum( vvect .* vvect ) );
+  
+  if ( !hit && ( dist(step) < rimp ) )
+    hitstep = step;
+    hit = true;
+  end
+
+  tabs += dt;
+  t(step) = tabs;
+end
+
+figure(1)
+plot( pos(:,1), pos(:,2), '1.');
+hold on
+plot( pos(nsteps,1), pos(nsteps,2), '1o');
+plot( pos(hitstep,1), pos(hitstep,2), '1x');
+title( 'trajectories [cm]' );
+plot( pos(:,3), pos(:,4), '3.');
+plot( pos(nsteps,3), pos(nsteps,4), '3o');
+plot( pos(hitstep,3), pos(hitstep,4), '3x');
+axis('square');
+hold off
+
+figure(2)
+plot( t, dist, '1.' );
+hold on
+plot( t(hitstep), dist(hitstep), '1x');
+title( 'distance [cm]' );
+hold off
+
+figure(3)
+plot( t, vrel, '1.' );
+hold on
+plot( t(hitstep), vrel(hitstep), '1x');
+title( 'relative velocity [cm/s]' );
+hold off
