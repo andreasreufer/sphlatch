35b5095d78fb78686248588a29870ca9e61e7731
Andreas Reufer <areufer@sacerdos.unibe.ch>
2009-09-18 15:58:21 +0200

    changes

--- tools/mars_postproc/postproc.sh
+++ tools/mars_postproc/postproc.sh
@@ -7,6 +7,25 @@ then
 fi
 source config.sh
 
+if [ "$SURFDIM" = "2.0m" ]
+then
+  ZOOM="4"
+  PTSIZE="0.25"
+  SMOLEN="1.7"
+fi
+if [ "$SURFDIM" = "4.0m" ]
+then
+  ZOOM="2"
+  PTSIZE="0.50"
+  SMOLEN="3.5"
+fi
+if [ "$SURFDIM" = "8.0m" ]
+then
+  ZOOM="1"
+  PTSIZE="1.00"
+  SMOLEN="7.0"
+fi
+
 
 writeOctScript()
 {
@@ -45,57 +64,48 @@ FILES=`find . -maxdepth 1 -iname 'out???.xdr' | sort`
 for FILE in $FILES
 do
     FILEPRFX=`echo $FILE|sed -e "s/\.xdr//"|sed -e "s/\.\///"`
-    
+
     # convert to h5part
-    if [ ! -e ${FILEPRFX}.h5part ]
+    if [ ! -e ${FILEPRFX}.h5part ] && [ ! -e ${FILEPRFX}.lock ]
     then
-      touch ${FILEPRFX}.h5part
+      touch ${FILEPRFX}.lock
+      echo "${FILEPRFX}.xdr -> ${FILEPRFX}.h5part"
       sleep 30
       cdat2h5part -i ${FILEPRFX}.xdr -o ${FILEPRFX}.h5part
       h5part_add_ANEOSout_table -i ${FILEPRFX}.h5part
       #h5part_get_phase_histogram -i ${FILEPRFX}.h5part -o ice_phases.txt -n 2
       #h5part_get_phase_histogram -i ${FILEPRFX}.h5part -o dun_phases.txt -n 4
+      rm ${FILEPRFX}.lock
     fi
     
     # convert to txt
-    if [ ! -e ${FILEPRFX}_cut.txt ]
+    if [ ! -e ${FILEPRFX}_cut.txt ] && [ -e ${FILEPRFX}.h5part ] && [ ! -e ${FILEPRFX}.lock ]
     then
-      touch ${FILEPRFX}_cut.txt
+      touch ${FILEPRFX}.lock
+      echo "${FILEPRFX}.h5part -> ${FILEPRFX}_cut.txt"
       OCTSCRIPT=`mktemp ./octXXXXXX`
-      writeConverterScript $OCTSCRIPT
-      h5part_slice --ymin=-3.5 --ymax=3.5 -i ${FILEPRFX}.h5part -o ${FILEPRFX}_cut.h5part
+      writeOctScript $OCTSCRIPT
+      h5part_slice --ymin=-${SMOLEN} --ymax=${SMOLEN} -i ${FILEPRFX}.h5part -o ${FILEPRFX}_cut.h5part
       $OCTSCRIPT ${FILEPRFX}
       rm ${FILEPRFX}_cut.h5part $OCTSCRIPT
+      rm ${FILEPRFX}.lock
     fi
 
     # make plot
-    if [ ! -e gifs/${FILEPRFX}.gif ]
+    if [ ! -e gifs/${FILEPRFX}.gif ] && [ -e ${FILEPRFX}_cut.txt ] && [ ! -e ${FILEPRFX}.lock ]
     then
-      touch gifs/${FILEPRFX}.gif
-      if [ "$SURFDIM" = "2.0m" ]
-      then
-        ZOOM="4"
-        PTSIZE="0.2"
-      fi
-      if [ "$SURFDIM" = "4.0m" ]
-      then
-        ZOOM="2"
-        PTSIZE="0.4"
-      fi
-      if [ "$SURFDIM" = "8.0m" ]
-      then
-        ZOOM="1"
-        PTSIZE="0.8"
-      fi
-
+      touch ${FILEPRFX}.lock
+      echo "plot ${FILEPRFX}.gif"
       TIMESTR=`h5part_readattr -i $FILEPRFX.h5part -k TIME|cut -c 7-17`
       ./scatterplot.py ${FILEPRFX}_cut.txt $TIMESTR $ZOOM $PTSIZE
+      echo "${FILEPRFX}_cut.txt.png -> gifs/$FILEPRFX.gif"
       convert ${FILEPRFX}_cut.txt.png gifs/$FILEPRFX.gif
       rm ${FILEPRFX}_cut.txt.png
+      rm ${FILEPRFX}.lock
     fi
 done
 
-gifsicle --delay 5 --colors 256 gifs/out*.gif  >scatter.gif
+gifsicle --delay 5 --colors 256 gifs/out*.gif  >${SIMNAME}.gif
 
 echo "finishing"
 exit 0
