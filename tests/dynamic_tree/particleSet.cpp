0af32ca74d2f8a7ed83a039d5ccd4714231fba95
Andreas Reufer <areufer@caliban.local>
2009-11-17 20:14:10 +0100

    IO works now

--- tests/dynamic_tree/particleSet.cpp
+++ tests/dynamic_tree/particleSet.cpp
@@ -29,18 +29,10 @@ typedef sphlatch::fType   fType;
 #include "sph_fluid_particle.h"
 #include "io_particle.h"
 
-class posPart {
-  public:
-    sphlatch::vect3dT      pos;
-    //size_t count[7];
-    //sphlatch::vect3dT           vel;
-};
-
 class particle :
-   public posPart,
-   /*public sphlatch::treePart,
+   public sphlatch::treePart,
    public sphlatch::movingPart,
-   public sphlatch::SPHfluidPart,*/
+   public sphlatch::SPHfluidPart,
    public sphlatch::IOPart
 {
 public:
@@ -50,11 +42,11 @@ public:
       ioVarLT vars;
 
       vars.push_back(storeVar(pos, "pos"));
-      /*vars.push_back(storeVar(vel, "vel"));
+      vars.push_back(storeVar(vel, "vel"));
       vars.push_back(storeVar(m, "m"));
       vars.push_back(storeVar(id, "id"));
       
-      vars.push_back(storeVar(h, "h"));*/
+      vars.push_back(storeVar(h, "h"));
 
       return(vars);
    }
@@ -65,10 +57,10 @@ public:
       ioVarLT vars;
 
       vars.push_back(storeVar(pos, "pos"));
-      /*vars.push_back(storeVar(vel, "vel"));
+      vars.push_back(storeVar(vel, "vel"));
       vars.push_back(storeVar(m, "m"));
       vars.push_back(storeVar(h, "h"));
-      vars.push_back(storeVar(id, "id"));*/
+      vars.push_back(storeVar(id, "id"));
 
       return(vars);
    }
@@ -87,81 +79,26 @@ typedef ghost                          ghstT;
 #include "particle_set.cpp"
 typedef sphlatch::ParticleSet<partT>   partSetT;
 
-/*#include "bhtree_treedump.cpp"
-   typedef sphlatch::BHTreeDump                   dumpT;
-
- #include "bhtree_worker_grav.cpp"
-   typedef sphlatch::fixThetaMAC                  macT;
-   typedef sphlatch::GravityWorker<macT, partT>   gravT;*/
-
-//#include "log_manager.h"
-//typedef sphlatch::LogManager                   logT;
-
-//#include "bhtree_worker_sphsum.cpp"
-
 
 int main(int argc, char* argv[])
 {
 #ifdef SPHLATCH_MPI
    MPI::Init(argc, argv);
 #endif
-   //treeT& Tree(treeT::instance());
 
    const fType  costMin = 1.0e4, costMax = 1.5e4;
-   //const size_t noParts = 1000000;
-   const size_t noParts = 100104;
-
-   //const size_t noParts = 300000;
-
-   //const fType costMin = 10., costMax = 15.;
-   //const size_t noParts = 100;
-
-   partSetT particles;
-   particles.resize(noParts);
    
-   for (size_t i = 0; i < noParts; i++)
-   {
-     particles[i].pos = 0,0,0;
-     //particles[i].vel = 0,0,0;
-   }
-
-   particles.loadHDF5("twophase.h5part");
-
-   particles.resize(100);
-   for (size_t i = 0; i < 20; i++)
-   {
-     std::cout << particles[i].pos << " " << &(particles[i].pos[0]) << "\n";
-     //std::cout << particles[i].vel << " " << &(particles[i].vel[0]) << "\n";
-     //std::cout <<  particles[i].id << "   " <<  particles[i].h << "   " <<  particles[i].m << "\n";
-   }
-     
-   //std::cout << particles[100103].pos << "\n\n\n";
-
-
-   particles.saveHDF5("out.h5part");
-
-
-   /*std::cout << particles.step << "\n";
-
-   for (size_t i = 0; i < noParts; i++)
-   {
-      particles[i].pos[0] = static_cast<fType>(rand()) / RAND_MAX;
-      particles[i].pos[1] = static_cast<fType>(rand()) / RAND_MAX;
-      particles[i].pos[2] = static_cast<fType>(rand()) / RAND_MAX;
-
-      particles[i].m = 1.;
-
-      particles[i].id   = i;
-      particles[i].cost = 1.;
-   }*/
+   partSetT particles;
 
    double start;
    start = omp_get_wtime();
-   for (size_t i = 0; i < noParts; i++)
-   {
-      //Tree.insertPart(particles[i]);
-   }
-   std::cout << "particles insert " << omp_get_wtime() - start << "s\n";
+   particles.loadHDF5("twophase.h5part");
+   std::cout << "particles loaded " << omp_get_wtime() - start << "s\n";
+   
+   
+   start = omp_get_wtime();
+   particles.saveHDF5("out.h5part");
+   std::cout << "particles stored " << omp_get_wtime() - start << "s\n";
 
 
 #ifdef SPHLATCH_MPI
