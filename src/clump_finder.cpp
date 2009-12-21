3cba9e336153209c9c01bdc696da45c7689c85d5
areufer <areufer@4efe6d44-883f-0410-9ed0-acde54f1c0bb>
2009-12-21 17:31:47 +0000

    clump finder added
    git-svn-id: https://isis2.unibe.ch/repos/sphlatch@230 4efe6d44-883f-0410-9ed0-acde54f1c0bb

---
+++ src/clump_finder.cpp
@@ -0,0 +1,18 @@
+#ifndef BHTREE_WORKER_CPP
+#define BHTREE_WORKER_CPP
+
+/*
+ *  clump_finder.cpp
+ *
+ *  Created by Andreas Reufer on 21.12.09
+ *  Copyright 2009 University of Berne. All rights reserved.
+ *
+ */
+
+
+#include "bhtree_worker.h"
+#include "bhtree.cpp"
+
+namespace sphlatch {
+};
+#endif
