8d0f0dd364c8d423168aa1ef36cd40a8466f2a8d
Andreas Reufer <andreas.reufer@space.unibe.ch>
2009-12-21 15:22:49 +0100

    changes

--- initials/lagrange_sphere1D/plotprofile.m
+++ initials/lagrange_sphere1D/plotprofile.m
@@ -13,18 +13,22 @@ color = strcat(num2str(color_idx),' ');
 prof = load(filename);
 
 subplot(2,2,1);
+hold on;
 plot( prof.r, prof.rho, color);
 title('density');
 
 subplot(2,2,2);
+hold on;
 plot( prof.r, prof.u, color);
 title('spec. energy');
 
 subplot(2,2,3);
+hold on;
 semilogy( prof.r, prof.p, color);
 title('pressure');
 
 subplot(2,2,4);
-plot( prof.r, 11700.*prof.T, color);
+hold on;
+plot( prof.r, 11604.*prof.T, color);
 title('material');
 
