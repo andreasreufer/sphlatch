6c59085de3f81b771de07c5a3f7354127678c427
Andreas Reufer <andreas.reufer@space.unibe.ch>
2010-01-22 18:13:10 +0100

    changes

--- src/bhtree_worker_sphsum.cpp
+++ src/bhtree_worker_sphsum.cpp
@@ -19,59 +19,71 @@ namespace sphlatch {
 template<typename _sumT, typename _partT>
 class SPHsumWorker : public NeighWorker<_sumT, _partT> {
 public:
-SPHsumWorker(const BHTreeWorker::treePtrT _treePtr) :
-  NeighWorker<_sumT, _partT>(_treePtr)
-{
-}
-SPHsumWorker(const SPHsumWorker& _SPHwork) :
-  NeighWorker<_sumT, _partT>(_SPHwork)
-{
-}
-~SPHsumWorker()
-{
-}
+   SPHsumWorker(const BHTreeWorker::treePtrT _treePtr) :
+      NeighWorker<_sumT, _partT>(_treePtr)
+   { }
 
-void operator()(const czllPtrT _czll);
+   SPHsumWorker(const SPHsumWorker& _SPHwork) :
+      NeighWorker<_sumT, _partT>(_SPHwork)
+   { }
 
-typedef sphlatch::Timer timerT;
+   ~SPHsumWorker()
+   { }
+
+   void operator()(const czllPtrT _czll);
+   void operator()(_partT* const _partPtr);
+
+   typedef sphlatch::Timer   timerT;
 
 private:
-timerT Timer;
+   timerT Timer;
 };
 
 
 template<typename _sumT, typename _partT>
 void SPHsumWorker<_sumT, _partT>::operator()(const czllPtrT _czll)
 {
-  nodePtrT curPart = _czll->chldFrst;
-  const nodePtrT stopChld = _czll->chldLast->next;
+   nodePtrT       curPart  = _czll->chldFrst;
+   const nodePtrT stopChld = _czll->chldLast->next;
 
-  // an empty CZ cell may have an chldFrst pointing to NULL
-  if (curPart == NULL)
-    return;
+   // an empty CZ cell may have an chldFrst pointing to NULL
+   if (curPart == NULL)
+      return;
 
-  Timer.start();
-  while (curPart != stopChld)
-    {
+   Timer.start();
+   while (curPart != stopChld)
+   {
       if (curPart->isParticle)
-        {
-          _partT* const partPtr = static_cast<_partT*>(
+      {
+         _partT* const partPtr = static_cast<_partT*>(
             static_cast<pnodPtrT>(curPart)->partPtr);
-          const fType hi = partPtr->h;
-          const fType srad = 2. * hi;
+         const fType hi   = partPtr->h;
+         const fType srad = 2. * hi;
 
-          NeighWorker<_sumT, _partT>::Func.preSum(partPtr);
-          NeighWorker<_sumT,
-                      _partT>::neighExecFunc(static_cast<pnodPtrT>(curPart),
-                                             srad);
-          NeighWorker<_sumT, _partT>::Func.postSum(partPtr);
-        }
+         NeighWorker<_sumT, _partT>::Func.preSum(partPtr);
+         NeighWorker<_sumT,
+                     _partT>::neighExecFunc(static_cast<pnodPtrT>(curPart),
+                                            srad);
+         NeighWorker<_sumT, _partT>::Func.postSum(partPtr);
+      }
       curPart = curPart->next;
-    }
-  const double compTime = Timer.getRoundTime();
-  _czll->compTime += static_cast<fType>(compTime);
+   }
+   const double compTime = Timer.getRoundTime();
+   _czll->compTime += static_cast<fType>(compTime);
+}
+
+template<typename _sumT, typename _partT>
+void SPHsumWorker<_sumT, _partT>::operator()(_partT* const _partPtr)
+{
+   pnodPtrT const pnodPtr = _partPtr->treeNode;
+
+   const fType hi   = _partPtr->h;
+   const fType srad = 2. * hi;
+
+   NeighWorker<_sumT, _partT>::Func.preSum(_partPtr);
+   NeighWorker<_sumT, _partT>::neighExecFunc(static_cast<pnodPtrT>(pnodPtr),
+                                             srad);
+   NeighWorker<_sumT, _partT>::Func.postSum(_partPtr);
 }
 };
-
-
 #endif
--- tools/sph_to_grid/sph2grid.cpp
+++ tools/sph_to_grid/sph2grid.cpp
@@ -45,10 +45,6 @@ typedef sphlatch::box3dT    box3dT;
 
 const fType finf = sphlatch::fTypeInf;
 
-#define SPHLATCH_LOGGER
-#include "logger.cpp"
-typedef sphlatch::Logger   logT;
-
 #include "bhtree.cpp"
 typedef sphlatch::BHTree   treeT;
 
@@ -58,7 +54,6 @@ typedef sphlatch::BHTree   treeT;
 #include "bhtree_particle.h"
 #include "sph_fluid_particle.h"
 #include "io_particle.h"
-#include "integrator_predcorr.cpp"
 
 class particle :
    public sphlatch::treePart,
@@ -74,62 +69,24 @@ class particle :
 #endif
 {
 public:
-   sphlatch::PredictorCorrectorO2<vect3dT> posInt;
-   sphlatch::PredictorCorrectorO1<fType>   energyInt;
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-   sphlatch::PredictorCorrectorO1<fType>   smolenInt;
-#endif
-
-   void bootstrap()
-   {
-      posInt.bootstrap(pos, vel, acc);
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-      energyInt.bootstrap(u, dudt);
-#endif
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-      smolenInt.bootstrap(h, dhdt);
-#endif
-   }
-
-   void predict(const fType _dt)
-   {
-      posInt.predict(pos, vel, acc, _dt);
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-      energyInt.predict(u, dudt, _dt);
-#endif
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-      smolenInt.predict(h, dhdt, _dt);
-#endif
-   }
-
-   void correct(const fType _dt)
-   {
-      posInt.correct(pos, vel, acc, _dt);
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-      energyInt.correct(u, dudt, _dt);
-#endif
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-      smolenInt.correct(h, dhdt, _dt);
-#endif
-   }
-
    ioVarLT getLoadVars()
    {
       ioVarLT vars;
 
+      // minimal
       vars.push_back(storeVar(pos, "pos"));
-      vars.push_back(storeVar(vel, "vel"));
       vars.push_back(storeVar(m, "m"));
       vars.push_back(storeVar(h, "h"));
       vars.push_back(storeVar(id, "id"));
 
       vars.push_back(storeVar(u, "u"));
-#ifdef SPHLATCH_ANEOS
-      vars.push_back(storeVar(mat, "mat"));
-#endif
-#ifdef SPHLATCH_GRAVITY_EPSSMOOTHING
-      vars.push_back(storeVar(eps, "eps"));
-#endif
+
+      if (not fName.empty())
+         vars.push_back(storeVar(f, fName));
+
+      if (not vName.empty())
+         vars.push_back(storeVar(v, vName));
+
       return(vars);
    }
 
@@ -137,49 +94,60 @@ public:
    {
       ioVarLT vars;
 
-      vars.push_back(storeVar(pos, "pos"));
-      vars.push_back(storeVar(vel, "vel"));
-      vars.push_back(storeVar(acc, "acc"));
-      vars.push_back(storeVar(m, "m"));
-      vars.push_back(storeVar(h, "h"));
-      vars.push_back(storeVar(id, "id"));
-      vars.push_back(storeVar(rho, "rho"));
-      vars.push_back(storeVar(noneigh, "noneigh"));
-
-      vars.push_back(storeVar(p, "p"));
-      vars.push_back(storeVar(u, "u"));
-      vars.push_back(storeVar(cs, "cs"));
-#ifdef SPHLATCH_ANEOS
-      vars.push_back(storeVar(mat, "mat"));
-      vars.push_back(storeVar(T, "T"));
-      vars.push_back(storeVar(phase, "phase"));
-#endif
-#ifdef SPHLATCH_GRAVITY_EPSSMOOTHING
-      vars.push_back(storeVar(eps, "eps"));
-#endif
-#ifdef SPHLATCH_VELDIV
-      vars.push_back(storeVar(divv, "divv"));
-#endif
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-      vars.push_back(storeVar(dudt, "dudt"));
-#endif
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-      vars.push_back(storeVar(dhdt, "dhdt"));
-#endif
-
-      vars.push_back(storeVar(cost, "cost"));
       return(vars);
+   }
+
+   static std::string fName, vName;
+
+public:
+   fType   f, ftar;
+   vect3dT v, vtar;
+
+   void setLoadF(std::string _str)
+   {
+      fName = _str;
+   }
+
+   void setLoadV(std::string _str)
+   {
+      vName = _str;
    }
 };
 
-typedef particle   partT;
+std::string particle::fName;
+std::string particle::vName;
 
-class ghost :
-   public sphlatch::treeGhost,
-   public sphlatch::movingGhost
-{ };
+template<typename _partT, typename _krnlT>
+struct interpolSum
+{
+   _krnlT  K;
 
-typedef ghost                                  ghstT;
+   void    preSum(_partT* const _i)
+   {}
+
+   void operator()(_partT* const _i,
+                   _partT* const _j,
+                   const vect3dT& _rvec,
+                   const fType _rr,
+                   const fType _srad)
+   {
+      const fType r  = sqrt(_rr);
+      const fType hj = _j->h;
+
+      const fType rhoi = _i->rho;
+      const fType mi   = _i->m;
+
+      const fType k = (K.value(r, hj)) * (mi / rhoi);
+
+      _j->ftar += k * _i->f;
+      _j->vtar += k * _i->v;
+   }
+
+   void postSum(_partT* const _i)
+   {}
+};
+
+typedef particle                               partT;
 
 #include "particle_set.cpp"
 typedef sphlatch::ParticleSet<partT>           partSetT;
@@ -194,248 +162,74 @@ typedef sphlatch::GravityWorker<macT, partT>   gravT;
 #include "sph_kernels.cpp"
 #include "bhtree_worker_sphsum.cpp"
 
-typedef sphlatch::CubicSpline3D                  krnlT;
+typedef sphlatch::CubicSpline3D                    krnlT;
 
 typedef sphlatch::densSum<partT, krnlT>          densT;
 typedef sphlatch::SPHsumWorker<densT, partT>     densSumT;
 
-typedef sphlatch::accPowSum<partT, krnlT>        accPowT;
-typedef sphlatch::SPHsumWorker<accPowT, partT>   accPowSumT;
+typedef interpolSum<partT, krnlT>        interpolSumT;
+typedef sphlatch::SPHsumWorker<interpolSumT, partT>   interpolWorkT;
 
 #include "bhtree_worker_cost.cpp"
-typedef sphlatch::CostWorker<partT>              costT;
+typedef sphlatch::CostWorker<partT>                costT;
 
 #ifdef SPHLATCH_ANEOS
  #include "eos_aneos.cpp"
-typedef sphlatch::ANEOS<partT>                   eosT;
+typedef sphlatch::ANEOS<partT>                     eosT;
 #else
  #include "eos_idealgas.cpp"
-typedef sphlatch::IdealGas<partT>                eosT;
+typedef sphlatch::IdealGas<partT>                  eosT;
 #endif
 
-#ifdef SPHLATCH_KEEPENERGYPROFILE
-#include "lookup_table1D.cpp"
-typedef sphlatch::InterpolateLinear              intplT;
-typedef sphlatch::LookupTable1D<intplT>          engLUT;
-
-engLUT energyLUT("profile1D.hdf5", "r", "u");
-#endif
 
 // particles are global
 partSetT parts;
+partSetT gridParts;
 
 
 
 void derive()
 {
    treeT& Tree(treeT::instance());
-   logT&  Logger(logT::instance());
 
-   const size_t nop       = parts.getNop();
-   const fType  costppart = 1. / nop;
+   const int nop       = parts.getNop();
+   const size_t nogp       = gridParts.getNop();
+   
+   const fType  costppart = 1. / ( nop + nogp);
 
    Tree.setExtent(parts.getBox() * 1.1);
 
-   for (size_t i = 0; i < nop; i++)
+   for (int i = 0; i < nop; i++)
    {
       parts[i].cost = costppart;
       Tree.insertPart(parts[i]);
    }
-   Logger << "created tree";
+   
+   for (size_t i = 0; i < nogp; i++)
+   {
+      gridParts[i].cost = costppart;
+      gridParts[i].m = 0.;
+      gridParts[i].rho = 1.;
+      Tree.insertPart(gridParts[i]);
+   }
+   std::cout << "created tree";
 
    Tree.update(0.8, 1.2);
 
-   treeT::czllPtrVectT CZbottomLoc   = Tree.getCZbottomLoc();
-   const int           noCZbottomLoc = CZbottomLoc.size();
-
-   Logger.stream << "Tree.update() -> " << noCZbottomLoc << " CZ cells";
-   Logger.flushStream();
-
-   //const size_t nop = parts.getNop();
-   for (size_t i = 0; i < nop; i++)
-   {
-      parts[i].acc = 0., 0., 0.;
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-      parts[i].dudt = 0.;
-#endif
-   }
-
-#ifdef SPHLATCH_GRAVITY
-   const fType G = parts.attributes["gravconst"];
-   gravT       gravWorker(&Tree, G);
- #pragma omp parallel for firstprivate(gravWorker)
-   for (int i = 0; i < noCZbottomLoc; i++)
-      gravWorker.calcGravity(CZbottomLoc[i]);
-   Logger << "Tree.calcGravity()";
-#endif
-
    densSumT densWorker(&Tree);
 #pragma omp parallel for firstprivate(densWorker)
-   for (int i = 0; i < noCZbottomLoc; i++)
-      densWorker(CZbottomLoc[i]);
-   Logger << "Tree.densWorker()";
+   for (int i = 0; i < nop; i++)
+      densWorker(&(parts[i]));
+   std::cout << "Tree.densWorker()";
 
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-   const fType uMin = parts.attributes["umin"];
-   Logger.stream << "assure minimal spec. energy umin = " << uMin;
-   Logger.flushStream();
-#endif
-
-   eosT& EOS(eosT::instance());
-   for (size_t i = 0; i < nop; i++)
-   {
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-      if (parts[i].u < uMin)
-         parts[i].u = uMin;
-#endif
-      EOS(parts[i]);
-   }
-   Logger << "pressure";
-
-   accPowSumT accPowWorker(&Tree);
-#pragma omp parallel for firstprivate(accPowWorker)
-   for (int i = 0; i < noCZbottomLoc; i++)
-      accPowWorker(CZbottomLoc[i]);
-   Logger << "Tree.accPowWorker()";
-
-#ifdef SPHLATCH_VELDIV
-   for (size_t i = 0; i < nop; i++)
-      parts[i].setDivvMax();
-   Logger << "setDivvMax()";
-#endif
-
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-   for (size_t i = 0; i < nop; i++)
-      setDhDt(parts[i]);
-#endif
-
-#ifdef SPHLATCH_FRICTION
-   const fType fricCoeff = 1. / parts.attributes["frictime"];
-   for (size_t i = 0; i < nop; i++)
-      parts[i].acc -= parts[i].vel * fricCoeff;
-   Logger.stream << "friction (t_fric = " << 1. / fricCoeff << ")";
-   Logger.flushStream();
-#endif
-
-#ifdef SPHLATCH_KEEPENERGYPROFILE
-   vect3dT com;
-   com = 0., 0., 0.;
-   fType   totM = 0.;
-   for (size_t i = 0; i < nop; i++)
-   {
-      com  += parts[i].pos * parts[i].m;
-      totM += parts[i].m;
-   }
-   com /= totM;
-
-   Logger.stream << "center of mass: ["
-                 << com[0] << ","
-                 << com[1] << ","
-                 << com[2] << "]";
-   Logger.flushStream();
-
-   const fType thermFricCoeff = 1. / parts.attributes["frictime"];
-   for (size_t i = 0; i < nop; i++)
-   {
-      const vect3dT rveci  = parts[i].pos - com;
-      const fType   ri     = sqrt(dot(rveci, rveci));
-      const fType   utheoi = energyLUT(ri);
-
-      parts[i].dudt -= (parts[i].u - utheoi) * thermFricCoeff;
-   }
-#endif
-
-   costT costWorker(&Tree);
-#pragma omp parallel for firstprivate(costWorker)
-   for (int i = 0; i < noCZbottomLoc; i++)
-      costWorker(CZbottomLoc[i]);
-   Logger << "Tree.costWorker()";
+   interpolWorkT interpolWorker(&Tree);
+#pragma omp parallel for firstprivate(interpolWorker)
+   for (int i = 0; i < nop; i++)
+      interpolWorker(&(parts[i]));
+   std::cout << "Tree.interpolWorker()";
 
    Tree.clear();
-   Logger << "Tree.clear()";
-}
-
-fType timestep(const fType _stepTime)
-{
-   logT& Logger(logT::instance());
-
-   const fType courant = parts.attributes["courant"];
-   const fType time    = parts.attributes["time"];
-
-   fType dtSave = (floor((time / _stepTime) + 1.e-6) + 1.) * _stepTime - time;
-   fType dtA    = finf;
-   fType dtCFL  = finf;
-
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-   fType dtU = finf;
-#endif
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-   fType dtH = finf;
-#endif
-
-   const size_t nop = parts.getNop();
-   for (size_t i = 0; i < nop; i++)
-   {
-      const fType ai = sqrt(dot(parts[i].acc, parts[i].acc));
-      if (ai > 0.)
-      {
-         const fType dtAi = sqrt(parts[i].h / ai);
-         dtA = dtAi < dtA ? dtAi : dtA;
-      }
-
-      const fType dtCFLi = parts[i].h / parts[i].cs;
-      dtCFL = dtCFLi < dtCFL ? dtCFLi : dtCFL;
-
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-      const fType dudti = parts[i].dudt;
-      if (dudti < 0.)
-      {
-         const fType dtUi = -parts[i].u / dudti;
-         dtU = dtUi < dtU ? dtUi : dtU;
-      }
-#endif
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-      const fType dhdti = parts[i].dhdt;
-      if (dhdti < 0.)
-      {
-         const fType dtHi = -parts[i].h / dhdti;
-         dtH = dtHi < dtH ? dtHi : dtH;
-      }
-#endif
-   }
-
-   dtA   *= 0.5;
-   dtCFL *= courant;
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-   dtH *= 0.15;
-#endif
-
-   //FIXME: globally minimize all dts
-   fType dt = finf;
-
-   dt = dtSave < dt ? dtSave : dt;
-   dt = dtA < dt ? dtA : dt;
-   dt = dtCFL < dt ? dtCFL : dt;
-
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-   dt = dtU < dt ? dtU : dt;
-#endif
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-   dt = dtH < dt ? dtH : dt;
-#endif
-   Logger.stream << "dt: " << dt << " "
-                 << "dtSave: " << dtSave << " "
-                 << "dtA: " << dtA << " "
-                 << "dtCFL: " << dtCFL << " "
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-                 << "dtU: " << dtU << " "
-#endif
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-                 << "dtH: " << dtH << " "
-#endif
-                 << " ";
-   Logger.flushStream();
-   return(dt);
+   std::cout << "Tree.clear()";
 }
 
 int main(int argc, char* argv[])
@@ -444,144 +238,43 @@ int main(int argc, char* argv[])
    MPI::Init(argc, argv);
 #endif
 
-   if (not ((argc == 4) || (argc == 5)))
+   if (not ((argc == 3) || (argc == 4)))
    {
       std::cerr <<
-      "usage: simple_sph_XXXXXX <inputdump> <saveStepTime> <stopTime> (<numthreads>)\n";
+      "usage: sph2grid <inputdump> <outputFile> (<numthreads>)\n";
       return(1);
    }
 
 
    std::string inFilename = argv[1];
+   std::string outFilename = argv[2];
 
-   std::istringstream stepStr(argv[2]);
-   fType stepTime;
-   stepStr >> stepTime;
-
-   std::istringstream stopStr(argv[3]);
-   fType stopTime;
-   stopStr >> stopTime;
-
-   if (argc == 5)
+   if (argc == 4)
    {
-      std::istringstream threadStr(argv[4]);
+      std::istringstream threadStr(argv[3]);
       int numThreads;
       threadStr >> numThreads;
       omp_set_num_threads(numThreads);
    }
 
-   logT& Logger(logT::instance());
-
-   ///
-   /// log program compilation time
-   ///
-   Logger.stream << "executable compiled from " << __FILE__
-                 << " on " << __DATE__
-                 << " at " << __TIME__ << "\n\n"
-                 << "    features: \n"
-#ifdef SPHLATCH_GRAVITY
-                 << "     gravity  \n"
-#endif
-#ifdef SPHLATCH_TIMEDEP_SMOOTHING
-                 << "     time dependent smoothing length\n"
-#endif
-#ifdef SPHLATCH_TIMEDEP_ENERGY
-                 << "     time dependent specific energy\n"
-#endif
-#ifdef SPHLATCH_TILLOTSON
-                 << "     Tillotson EOS\n"
-#endif
-#ifdef SPHLATCH_ANEOS
-                 << "     ANEOS\n"
-#endif
-#ifdef SPHLATCH_INTEGRATERHO
-                 << "     integrated density\n"
-#endif
-#ifdef SPHLATCH_FRICTION
-                 << "     friction\n"
-#endif
-                 << "     basic SPH\n";
-   Logger.flushStream();
-
-   Logger.stream << "working on " << omp_get_num_threads() << " threads";
-   Logger.flushStream();
-
    // load the particles
    parts.loadHDF5(inFilename);
-   Logger << "loaded particles";
+   std::cout << "loaded particles";
 
    fType& time(parts.attributes["time"]);
-   cType& step(parts.step);
-   treeT& Tree(treeT::instance());
 
-   const size_t nop = parts.getNop();
-
-   parts[0].noneighOpt = 50;
+   gridParts.step = parts.step;
+   gridParts.attributes = parts.attributes;
 
    // first bootstrapping step
    derive();
-   for (size_t i = 0; i < nop; i++)
-      parts[i].bootstrap();
 
-   Logger.finishStep("bootstrapped integrator");
-
-   parts.doublePrecOut();
-   parts.saveHDF5("bootstrap.h5part");
-
-   // start the loop
-   while (time < stopTime)
-   {
-      derive();
-
-      const fType dt = timestep(stepTime);
-
-      for (size_t i = 0; i < nop; i++)
-         parts[i].predict(dt);
-      Logger.finishStep("predicted");
-
-      time += dt;
-      derive();
-
-      for (size_t i = 0; i < nop; i++)
-         parts[i].correct(dt);
-      step++;
-
-      std::stringstream sstr;
-      sstr << "corrected (t = " << time << ")";
-      Logger.finishStep(sstr.str());
-
-      const fType dumpTime = (floor((time / stepTime) + 1.e-9)) * stepTime;
-      if (fabs(dumpTime - time) < 1.e-9)
-      {
-         std::stringstream dumpStr, stepStr, timeStr;
-
-         dumpStr << "dump";
-
-         stepStr << step;
-         // pad step number to 7 digits
-         for (size_t i = stepStr.str().size(); i < 7; i++)
-            dumpStr << "0";
-         dumpStr << stepStr.str();
-
-         dumpStr << "_T";
-         timeStr << std::setprecision(4) << std::scientific << time;
-         // pad time string to 11 digits
-         for (size_t i = timeStr.str().size(); i < 11; i++)
-            dumpStr << "0";
-         dumpStr << timeStr.str();
-         dumpStr << ".h5part";
-
-         parts.saveHDF5(dumpStr.str());
-
-         Logger.stream << "write " << dumpStr.str();
-         Logger.flushStream();
-      }
-   }
-
-   Logger << "stored dump ";
+   gridParts.doublePrecOut();
+   gridParts.saveHDF5(outFilename);
 
 #ifdef SPHLATCH_MPI
    MPI::Finalize();
 #endif
    return(0);
 }
+
