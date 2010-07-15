c5a773a43c4be4c456164cca27542c7251ac29e1
Andreas Reufer <andreas.reufer@space.unibe.ch>
2010-07-15 22:00:54 +0200

    changes

---
+++ tools/py_tools/h5part.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import tables as pt
+
+class H5PartDump(object):
+  def __init__(self,fname):
+    self.fname = fname
+    self.pth = pt.openFile(fname, "r")
+
+  def __del__(self):
+    self.pth.close()
+
+  def getStep(self,sname="/current"):
+    pth = self.pth
+
+    for node in pth.walkNodes():
+      if node._v_pathname == sname:
+        if type(node) == pt.group.Group:
+          return node
+        else:
+          return self.getStep(node.target)
+    return []
+
+  def getStepNames(self):
+    stpnam = []
+    for node in self.pth.walkGroups():
+      if "/Step" in node._v_pathname:
+        stpnam.append(node._v_pathname)
+    return stpnam
+
+  def getAttrKeys(self):
+    pass
+
+  def getAttr(self,key):
+    pass
+
+
