9ca86e7023715e78d46ced0e5ebb9ff4bfe1eb1e
Andreas Reufer <andreas.reufer@space.unibe.ch>
2010-08-01 17:14:55 +0200

    changes

--- tools/gi_admin/gi_plot.py
+++ tools/gi_admin/gi_plot.py
@@ -5,12 +5,35 @@ import numpy as np
 import matplotlib as mp
 mp.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.colors as col
 
 from fewbody import FewBodies
 from h5part import H5PartDump
 
 #mp.rc('text', usetex=True)
 mp.rc('text.latex', preamble = '\usepackage{amssymb}, \usepackage{wasysym}')
+
+def cnameToRGB(str):
+  col.cnames[str]
+  return np.array( col.hex2color( col.cnames[str] ) )
+
+def colorBodyAndMat(pdump, cdump, filt, cfg):
+  cmap = np.zeros((2,6,3))
+  cmap[0,2] = cnameToRGB("blue")
+  cmap[0,4] = cnameToRGB("red")
+  cmap[0,5] = cnameToRGB("darkgrey")
+  cmap[1,2] = cnameToRGB("aqua")
+  cmap[1,4] = cnameToRGB("orange")
+  cmap[1,5] = cnameToRGB("grey")
+
+  mat = pdump.mat[:,0].compress(filt)[:]
+  bod = np.int32( (pdump.id[:].compress(filt)[:] > cfg.idfilt) )
+
+  color = cmap[bod, mat]
+  ptsize = cfg.ptsize
+
+  return (color, ptsize)
+
 
 class GIplotConfig(object):
   def __init__(self):
@@ -32,16 +55,7 @@ class GIplotConfig(object):
     self.zmax =  7.e7
 
     self.ds = 5.e7
-    self.ptsize = 0.10
-
-    cmap = np.zeros((2,6), float)
-    cmap[0,2] = 0.30 # ice
-    cmap[0,4] = 0.95 # dun
-    cmap[0,5] = 0.05 # iron
-    cmap[1,2] = 0.35
-    cmap[1,4] = 0.90
-    cmap[1,5] = 0.10
-    self.cmap = cmap
+    self.ptsize = 0.05
 
     self.axisbg = "k"
 
@@ -54,20 +68,22 @@ class GIplotConfig(object):
 
     self.Mearth = 5.9736e27
     self.Mmin    = 0.e-0*self.Mearth
-    self.MminLbl = 1.e-3*self.Mearth
+    self.MminLbl = 5.e-3*self.Mearth
 
-    self.clpc_ec = 'yellow'
+    self.colorFunc = colorBodyAndMat
+
+    self.clpc_ec = 'yellowgreen'
     self.clpc_fc = 'none'
     self.clpc_lw = 0.3
     self.clpc_al = 0.3
 
-    self.clpc_txtc = 'yellow'
-    self.clpc_txts = 4
-
-    self.clpp_ec = 'green'
+    self.clpp_ec = 'yellowgreen'
     self.clpp_fc = 'none'
     self.clpp_lw = 0.3
     self.clpp_al = 0.6
+    
+    self.clpc_txtc = 'yellowgreen'
+    self.clpc_txts = 4
 
     self.scal_fc = 'white'
     self.scal_vc = [0.1, 0.1, 0.3, 0.1]
@@ -84,8 +100,9 @@ class GIplotConfig(object):
     self.parm_fc = 'white'
     self.parm_vc = [0.8, 0.8]
     self.parm_txts = 4
+    self.parm_txt = ""
     self.copy_vc = [0.6, 0.1]
-    self.copy_txt = '$\mathrm{Andreas Reufer, University of Bern}$'
+    self.copy_txt = "Andreas Reufer, University of Bern"
 
     self.idfilt = 2.e6
 
@@ -116,59 +133,13 @@ class GIplot(object):
     self.scl = scl
     self.corrax = corrax
 
-  def plotTime(self, time):
-    cfg    = self.cfg
-    timetxt = '$t = %6.2f' % (time*cfg.time_sc) + ' ' + cfg.time_ut + '$'
-    self.norma.text( cfg.time_vc[0], cfg.time_vc[1], timetxt, \
-        color=cfg.time_fc, size=cfg.time_txts)
-
-  def plotSimParams(self, params):
-    cfg    = self.cfg
-    pos = cfg.parm_vc
-    col = cfg.parm_fc
-    siz = cfg.parm_txts
-
-    parmtxt = '$ haba haba2 haba3 $'
-    self.norma.text( pos[0], pos[1], parmtxt, \
-        color=cfg.parm_fc, size=cfg.parm_txts )
-    
-    poscopy = cfg.copy_vc
-    self.norma.text( poscopy[0], poscopy[1], cfg.copy_txt, \
-        color=cfg.parm_fc, size=cfg.parm_txts )
-
-  def plotClumps(self,cfile):
-    cfg    = self.cfg
-    clumps = self.clumps
-    pax = self.physa
-
-    #print "integrate clumps ...   "
-    if cfg.plottraj:
-      clumps.integrate(dt, cfg.ds)
-    
-    #print "plotting clumps with trajectories ... "
-    for i in range(clumps.noc):
-      curtraj = clumps.traj[i]
-      pax.add_patch( mp.patches.Circle((curtraj[0,cfg.X], curtraj[0,cfg.Y]),\
-          radius=clumps.rc[i], ec=cfg.clpc_ec, fc=cfg.clpc_fc, \
-          lw=cfg.clpc_lw, alpha=cfg.clpc_al) )
-    
-      if ( clumps.m[i] > cfg.MminLbl ):
-        pax.text( curtraj[0,cfg.X], curtraj[0,cfg.Y], \
-            '$\mathrm{'+ '%1.4f' % ( clumps.m[i] / cfg.Mearth ) +' M_{E}}$', \
-            size=cfg.clpc_txts, color=cfg.clpc_txtc)
-    
-      if cfg.plottraj:
-        pax.add_patch( \
-            mp.patches.Circle((curtraj[-1,cfg.X], curtraj[-1,cfg.Y]), \
-            radius=clumps.rc[i], ec=cfg.clpp_ec, fc=cfg.clpp_fc, \
-            lw=cfg.clpp_lw, alpha=cfg.clpp_al) )
-        pax.plot( curtraj[:,cfg.X], curtraj[:,cfg.Y], color=cfg.clpt_fc, \
-            lw=cfg.clpt_lw, alpha=cfg.clpt_al)
-
+  
   def plotParticles(self,pfile,cfile, ifile):
     fig = self.fig
 
     cfg = self.cfg
+    pax = self.physa
+    nax = self.norma
 
     print "loading particles ..."
     pdumpf = H5PartDump(pfile)
@@ -190,7 +161,7 @@ class GIplot(object):
     cposz = np.zeros( max( cdump.id[:,0] ) + 1)
     for i in range(noc):
       cposz[ int(cdump.id[i]) ] = cdump.pos[i,cfg.Z]
-    
+
     print "selecting particles ..."
     h = pdump.h
     cid = pdump.clumpid
@@ -211,30 +182,71 @@ class GIplot(object):
     bod = np.int32( (pdump.id[:].compress(filt)[:] > cfg.idfilt) )
 
     print "z-sorting points ...   "
-    color = cfg.cmap[bod, mat]
     sidx = pos[:,cfg.Z].argsort()
     
-    print "plotting points ...   "
-    self.physa.scatter( pos[sidx,cfg.X], pos[sidx,cfg.Y], cfg.ptsize, \
-        color[sidx], lw=0, vmin=0., vmax=1.)
+    if cfg.plottraj:
+      print "integrate clumps ...   "
+      clumps.integrate(dt, cfg.ds)
+    
+    print "plotting clumps with trajectories ... "
+    for i in range(1,noc):
+      curtraj = clumps.traj[i]
+      pax.add_patch( mp.patches.Circle((curtraj[0,cfg.X], curtraj[0,cfg.Y]),\
+          radius=clumps.rc[i], ec=cfg.clpc_ec, fc=cfg.clpc_fc, \
+          lw=cfg.clpc_lw, alpha=cfg.clpc_al) )
+    
+      if ( clumps.m[i] > cfg.MminLbl ):
+        pax.text( curtraj[0,cfg.X], curtraj[0,cfg.Y], \
+            '$\mathrm{'+ '%1.4f' % ( clumps.m[i] / cfg.Mearth ) +' M_{E}}$', \
+            size=cfg.clpc_txts, color=cfg.clpc_txtc)
+    
+      if cfg.plottraj:
+        pax.add_patch( \
+            mp.patches.Circle((curtraj[-1,cfg.X], curtraj[-1,cfg.Y]), \
+            radius=clumps.rc[i], ec=cfg.clpp_ec, fc=cfg.clpp_fc, \
+            lw=cfg.clpp_lw, alpha=cfg.clpp_al) )
+        pax.plot( curtraj[:,cfg.X], curtraj[:,cfg.Y], color=cfg.clpt_fc, \
+            lw=cfg.clpt_lw, alpha=cfg.clpt_al)
 
-    self.physa.axis("scaled")
-    self.physa.axis(self.corrax)
+    print "get point colors and size"
+    (pcol, ptsize) = cfg.colorFunc(pdump, cdump, filt, cfg)
+    
+    print "plotting points ...   "
+#    pax.scatter( pos[sidx,cfg.X], pos[sidx,cfg.Y], cfg.ptsize, \
+#        color[sidx], lw=0, vmin=0., vmax=1.)
+    
+    pax.scatter( pos[sidx,cfg.X], pos[sidx,cfg.Y], ptsize, \
+        pcol[sidx,:], lw=0, vmin=0., vmax=1.)
+
+    pax.axis("scaled")
+    pax.axis(self.corrax)
    
     # plot a scale
     print "plot a scale    ...   "
-    self.norma.plot( [cfg.scal_vc[0], cfg.scal_vc[2]], \
+    nax.plot( [cfg.scal_vc[0], cfg.scal_vc[2]], \
         [cfg.scal_vc[1], cfg.scal_vc[3]], lw=.3, color=cfg.scal_fc )
     
-    self.norma.axis([0., 1., 0., 1.])
+    nax.axis([0., 1., 0., 1.])
+    
+    print "plot time ..."
+    timetxt = '$t = %6.2f' % (time*cfg.time_sc) + ' ' + cfg.time_ut + '$'
+    nax.text( cfg.time_vc[0], cfg.time_vc[1], timetxt, \
+        color=cfg.time_fc, size=cfg.time_txts)
     
     scldst = cfg.scal_vc[2] - cfg.scal_vc[0]
     scltxt = '$%6.1e' % (scldst*self.scl*cfg.yinch*cfg.scal_sc) \
         + "} "+ cfg.scal_ut + '$'
     scltxt = scltxt.replace('e+0', 'e+')
     scltxt = scltxt.replace('e+', '*10^{')
-    self.norma.text( cfg.scal_vc[0], cfg.scal_vc[1] + 0.01, scltxt, \
+    nax.text( cfg.scal_vc[0], cfg.scal_vc[1] + 0.01, scltxt, \
         color=cfg.scal_fc, size=cfg.scal_txts)
+    
+    #print "parameters and copyright ..."
+    self.norma.text( cfg.parm_vc[0], cfg.parm_vc[1], cfg.parm_txt, \
+        color=cfg.parm_fc, size=cfg.parm_txts )
+    
+    self.norma.text( cfg.copy_vc[0], cfg.copy_vc[1], cfg.copy_txt, \
+        color=cfg.parm_fc, size=cfg.parm_txts )
 
     # save the figure
     print "save figure     ...   "
