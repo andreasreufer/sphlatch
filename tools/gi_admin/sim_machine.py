c2d45e28c2dc2b2577b937ee194c351acfd410c3
[ Andreas REUFER -  - B1702 - Academic Prof Hire - SPH models of planet-forming collisions - ASURITE - 03/08/2013 ] <andreas.reufer@asu.edu>
2013-03-26 12:53:56 -0700

    changes

--- tools/gi_admin/sim_machine.py
+++ tools/gi_admin/sim_machine.py
@@ -31,11 +31,7 @@ qsub_sgl = "qsub $JOBCMD"
 qsub_sglscr = "#!/bin/bash\n\n#PBS -l walltime=95:59:59\n\nsource ~/.profile\ncd $PBS_O_WORKDIR\n\n$JOBCMD"
 python   = "/home/areufer/bin/python2.7"
 qsub_omp = "qsub $SCRIPT"
-<<<<<<< HEAD
-qsub_scr = "#!/bin/bash\n\n#PBS -l walltime=96:00:00\n#PBS -l nodes=1:ppn=$NOCPUS\n#PBS -N $SIMNAME\n\nsource ~/.profile\ncd $PBS_O_WORKDIR\n./$BINARY initial.h5part $SAVETIME $STOPTIME $RUNARGS"
-=======
-qsub_ompscr = "#!/bin/bash\n\n#PBS -l walltime=95:59:59\n#PBS -l nodes=1:ppn=$NOCPUS\n#PBS -N $SIMNAME\n\nsource ~/.profile\ncd $PBS_O_WORKDIR\n./$BINARY initial.h5part $SAVETIME $STOPTIME $RUNARGS\n"
->>>>>>> b1581de17111b49709a0ab02827180d3bb0062a9
+qsub_ompscr = "#!/bin/bash\n\n#PBS -l walltime=05:59:59\n#PBS -l nodes=1:ppn=$NOCPUS\n#PBS -N $SIMNAME\n\nsource ~/.profile\ncd $PBS_O_WORKDIR\n./$BINARY initial.h5part $SAVETIME $STOPTIME $RUNARGS\n"
 qdel     = "qdel $JOBID"
 basedir  = "/home/areufer/"
 srcdir   = basedir + "repos/sphlatch/apps/simple_sph"
